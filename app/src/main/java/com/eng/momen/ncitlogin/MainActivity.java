42c517005a0d2c6504f468c5bd94d2fee4c3e78e
Momen <engmomenali@gmail.com>
2020-03-31 10:51:35 +0200

    add the strusture of the program
    the main structure of the program

---
+++ app/src/main/java/com/eng/momen/ncitlogin/MainActivity.java
@@ -0,0 +1,16 @@
+package com.eng.momen.ncitlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.eng.momen.ncitlogin.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/eng/momen/ncitlogin/entry/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.eng.momen.ncitlogin.entry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.eng.momen.ncitlogin.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
