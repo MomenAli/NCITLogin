0055347fd6b0c68244944bf2da97b38424d25d83
Momen <engmomenali@gmail.com>
2020-03-31 10:52:42 +0200

    add the first activity
    this activity will be the entry activity if the user didn't login before

---
+++ app/src/main/java/com/eng/momen/ncitlogin/entry/FirstActivity.java
@@ -0,0 +1,24 @@
+package com.eng.momen.ncitlogin.entry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.eng.momen.ncitlogin.R;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+
+    public void login(View view) {
+        //Intent intent =  new Intent(this,Lo)
+    }
+
+    public void register(View view) {
+    }
+}
